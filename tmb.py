8bad78537171af35e7effe404ca6e8185ee1ab18
tmbuza <ndelly@gmail.com>
2022-09-12 00:39:46 -0500

    Update tmb.py

--- tmb.py
+++ tmb.py
@@ -117,52 +117,11 @@ with st.container():
           ### Machine Learning & Microbiome
           ...are fields getting lots of attention recently. PubMed metrics can prove this theory!
           """)
-        st.image(pubmed_stats)
-        
-        ##################################
-        # Code and Image
-        ##################################
-        # with st.container():
-        process = subprocess.Popen(["Rscript", "search_pubmed.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        result = process.communicate()
         image = Image.open("figures/pubmed_search_bar_plot.png")
         st.image(image)
         st.caption("This line chart was created using an `R script`, then imported into this web app generated using `streamlit` library and `Python`. Integration of different tools demonstrates robust solutions for gaining insights into complex data.")
         # st.subheader("Creating a plot using `ggplot2`")
         with st.expander("Click to see or copy the R code"):
-          # code ="""
-          #   library(tidyverse)
-          #   
-          #   # Importing preprocessed Pubmed searches
-          #   read_csv("data/search_counts.csv", show_col_types = TRUE) %>% 
-          #     select(year, ends_with("_res")) %>%  
-          #     filter(year >= 1991) %>% # Filter as you please!
-          #     mutate(
-          #       Microbiome = 100 * (microb_res / all_res),
-          #       Genomics = 100 * (genom_res / all_res),
-          #       Proteomics = 100 * (proteo_res / all_res),
-          #       Metabolomics = 100 * (metabol_res / all_res),
-          #       Lipidomics = 100 * (lipid_res / all_res),
-          #       Phenomics = 100 * (pheno_res / all_res),
-          #       Transcriptomics = 100 * (transcr_res / all_res),
-          #       MachineLearning = 100 * (ml_res / all_res),
-          #       Bioinformatics = 100 * (bioinfo_res / all_res),
-          #       Pharmacogenomics = 100 * (pharma_res / all_res),
-          #       AllArticles = all_res) %>% 
-          #     select(-AllArticles, -ends_with("_res")) %>% 
-          #     pivot_longer(-year) %>%
-          #     filter(value > 0) %>% 
-          #     ggplot(aes(x = year, y = value, group = name, color = name)) +
-          #     geom_line(size = 1.2) +
-          #     
-          #     # Optional color scaling
-          #     scale_color_manual(name = "FIELD",
-          #                        values = c("red", "green4", "gray", "orange", "blue4", "green2", "magenta", "purple", "maroon", "blue1")) +
-          #     labs(x = "Year", y = "Percentage of articles in PubMed", color = "FIELD") +
-          #     theme_classic()
-          #   
-          #   ggsave("figures/pubmed_search_bar_plot.png", width = 8, height = 5)
-          # """
           st.code(pub_search_code, language="R")
     
     
